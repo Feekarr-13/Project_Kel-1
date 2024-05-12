dc4087fbeb7d0f6299b2b552577fc32a493d2e07
Muhammad Fikar Firdiansyah <fikar030204@gmail.com>
2024-05-12 13:37:30 +0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Catalog.css
    	new file:   Catalog.html
    	new file:   Home.css
    	new file:   Home.html
    	modified:   Profile.jpg
    	new file:   Vidio.mp4
    	new file:   gambar6.jpeg
    	modified:   index.html
    	new file:   index.js
    	modified:   styles.css

---
+++ index.js
@@ -0,0 +1,7 @@
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+    themeToggle.classList.toggle('dark-mode');
+});
